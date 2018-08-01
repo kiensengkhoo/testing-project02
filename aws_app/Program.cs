0a91405f46039c40dccebd577c23ecff7b19b054
kiensengkhoo <37057384+kiensengkhoo@users.noreply.github.com>
2018-08-01 17:35:38 +0800

    new app

---
+++ aws_app/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace aws_app
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Input:");
+            while(true)
+            {
+                string value = Console.ReadLine();
+                Console.WriteLine("Input value:" + value);
+            }
+        }
+    }
+}
