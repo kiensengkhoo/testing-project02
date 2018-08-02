b9129ef808d2c1633d0ca84f400280a748ed2bac
kiensengkhoo <37057384+kiensengkhoo@users.noreply.github.com>
2018-08-02 11:02:17 +0800

    new TCP

--- aws_app/Program.cs
+++ aws_app/Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
 
 namespace aws_app
 {
@@ -6,12 +9,41 @@ namespace aws_app
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Input:");
-            while(true)
+            Console.WriteLine("IP Address:" + IPAddress.Any);
+            System.Net.IPAddress theIPAddress;
+            theIPAddress = System.Net.IPAddress.Parse("127.0.0.1");
+            
+            TcpListener myTcpListener = new TcpListener(IPAddress.Any, 8080);
+            myTcpListener.Start();
+            Console.WriteLine("Port 8080 waiting client connect...");
+            Socket mySocket = myTcpListener.AcceptSocket();
+            do
             {
-                string value = Console.ReadLine();
-                Console.WriteLine("Input value:" + value);
-            }
+                try
+                {
+                    if (mySocket.Connected)
+                    {
+                        int dataLength;
+                        Console.WriteLine("Connect Success...");
+                        byte[] myBufferBytes = new byte[1000];
+                        dataLength = mySocket.Receive(myBufferBytes);
+
+                        Console.WriteLine("Received data lenght {0} \n ", dataLength.ToString());
+                        Console.WriteLine("Get Input Context:");
+                        Console.WriteLine(Encoding.ASCII.GetString(myBufferBytes, 0, dataLength) + "\n");
+                        Console.WriteLine("Input [Any] return data to client :");
+                        Console.ReadLine();
+                        mySocket.Send(myBufferBytes, myBufferBytes.Length, 0);
+                    }
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e.Message);
+                    mySocket.Close();
+                    break;
+                }
+
+            } while (true);
         }
     }
 }
