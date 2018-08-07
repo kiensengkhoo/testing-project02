f2bc86b87c9b2e129c7fe838a83a4a3b5bc1295e
kiensengkhoo <37057384+kiensengkhoo@users.noreply.github.com>
2018-08-07 11:57:09 +0800

    update new code

---
+++ App.ServerVersion3/Program.cs
@@ -0,0 +1,15 @@
+﻿using App.ServerVersion3.Services;
+using System;
+using System.Net.Sockets;
+
+namespace App.ServerVersion3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TCPServer server = new TCPServer();
+            server.AcceptPeersAsync();
+        }
+    }
+}
---
+++ App.ServerVersion3/Services/TCPClient.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace App.ServerVersion3.Services
+{
+    public class TCPClient
+    {
+
+    }
+}
